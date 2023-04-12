4a46ce6c1eacd1fd389587532106ef604182d230
Mohamed Kamal <medoking91@gmail.com>
2023-04-12 19:39:46 +0200

    feat: non blocking send/recv using threads

--- chat.py
+++ chat.py
@@ -2,76 +2,85 @@
 import RSA
 import socket
 import pickle
+import threading
+
+
+def exchange_keys(sock, rsa):
+    # generate keys
+    keys = rsa.generate_keys()
+    # the server/client sends the public key to the client
+    # the server/client receives the public key from the client
+    sock.send(pickle.dumps(keys[1]))  # the e value
+    msg = sock.recv(2048)
+    e = pickle.loads(msg)
+    sock.send(pickle.dumps(keys[0]))  # the n value
+    msg = sock.recv(2048)
+    n = pickle.loads(msg)
+
+    # set the public key of the other instance
+    rsa.set_public_key(n, e)
+    print("Received Public key:", (n, e))
+
+
+def sock_connect(sock, port=4321):
+    # create socket object if it was not created then this instance is a server
+    try:
+        sock.bind(("localhost", port))
+        sock.listen()
+        client, _ = sock.accept()
+    except:
+        # port was open so this instance is a client
+        sock.connect(("localhost", port))
+        client = sock
+
+    return client
+
+
+def send_message(sock, rsa):
+    while True:
+        # read the message
+        msg = input("Enter message: ")
+        # encode the message
+        msg = rsa.encode(msg)
+        msg = pickle.dumps(msg)
+        sock.send(msg)
+
+
+def recv_message(sock, rsa):
+    while True:
+        msg = sock.recv(4096)
+        msg = pickle.loads(msg)
+        # decode the message
+        msg = rsa.decode(msg)
+        print(msg)
 
 
 def main():
     # create socket object
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # connect to the server/client
+    sock = sock_connect(sock)
+
     # generate RSA keys
     rsa = RSA.RSA()
-    keys = rsa.generate_keys()
-    try:
-        # create socket object if it was not created then this instance is a server
-        sock.bind(("localhost", 4321))
-        sock.listen()
-        client, _ = sock.accept()
-        # the server sends the public key to the client
-        # the server receives the public key from the client
-        client.send(pickle.dumps(keys[1]))  # the e value
-        msg = client.recv(2048)
-        e = pickle.loads(msg)
-        client.send(pickle.dumps(keys[0]))  # the n value
-        msg = client.recv(2048)
-        n = pickle.loads(msg)
 
-        # set the public key of the other instance
-        rsa.set_public_key(n, e)
-        print("Public keys:", (n, e))
+    # exchange keys
+    exchange_keys(sock, rsa)
 
-        while 1:
-            msg = input("Enter message: ")
-            msg = rsa.encode(msg)
-            msg = pickle.dumps(msg)
-            client.send(msg)
+    # creating threads
+    # Send message thread
+    t1 = threading.Thread(target=send_message, args=(sock, rsa), name="t1")
+    # Receive message thread
+    t2 = threading.Thread(target=recv_message, args=(sock, rsa), name="t2")
 
-            msg = client.recv(4096)
-            msg = pickle.loads(msg)
-            # decode the message
-            msg = rsa.decode(msg)
-            print(msg)
+    # starting threads
+    t1.start()
+    t2.start()
 
-    except:
-        # port was open so this instance is a client
-        sock.connect(("localhost", 4321))
-        client = sock
-        # the client receives the public key from the server
-        # the client sends the public key to the server
-
-        msg = client.recv(2048)
-        e = pickle.loads(msg)
-        client.send(pickle.dumps(keys[1]))  # the e value
-
-        msg = client.recv(2048)
-        n = pickle.loads(msg)
-        client.send(pickle.dumps(keys[0]))  # the n value
-
-        # set the public key of the other instance
-        rsa.set_public_key(n, e)
-
-        # public keys
-        print("Public keys:", (n, e))
-
-        while 1:
-            msg = client.recv(4096)
-            msg = pickle.loads(msg)
-            # decode the message
-            msg = rsa.decode(msg)
-            print(msg)
-
-            msg = input("Enter message: ")
-            msg = rsa.encode(msg)
-            msg = pickle.dumps(msg)
-            client.send(msg)
+    # wait until all threads finish
+    t1.join()
+    t2.join()
 
     # close the socket
     sock.close()
